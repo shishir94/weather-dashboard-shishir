6c9fdfbee09f0d9db2f822a181eadeae1d573373
Shishir Kushwah <shishirkushwah@gmail.com>
2025-07-02 15:23:44 +0530

    adding loader

--- src/loader.js
+++ src/loader.js
@@ -1,16 +1,7 @@
-import { TailSpin } from "react-loader-spinner";
-const LoaderComp = () => {
+export default function Loader() {
     return (
-        <TailSpin
-            height="80"
-            width="80"
-            color="#4fa94d"
-            ariaLabel="tail-spin-loading"
-            radius="1"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-        />
+      <div className="flex justify-center items-center py-6">
+        <div className="w-8 h-8 border-4 border-blue-200 border-t-transparent rounded-full animate-spin"></div>
+      </div>
     );
-};  
-export default LoaderComp;
+  }
